1c4c97063dff3a4161f8d0cf73d1371725384410
mason ashlock <masonashlock@gmail.com>
2018-12-21 13:00:27 -0600

    current

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,24 @@
-var diff = [30,20,10] //TODO: Us jQuery to set css animate && timer****
+var diff = [30, 20, 10] //TODO: Us jQuery to set css animate && timer****
 var timerOn = true;
+var catagory;
 
 $(document).ready(function () {
+
+    $(".radio").click(function () {
+        catagory = $(this).attr("id");
+        console.log(catagory)
+    });
+
+    $("#")
+
+    $("#start").click(function() {
+
+        $.getJSON()
+
+    });
+
+
+
     var countdownNumberEl = $('#countdown-number');
     var countdown = 45;
     countdownNumberEl.text(countdown);
@@ -15,5 +32,8 @@ $(document).ready(function () {
             countdown = --countdown <= 0 ? 45 : countdown;
         }
     }, 1000);
+
+    $(".ans").on("click", function () {
+    });
 
 });
